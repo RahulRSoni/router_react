808823f2c3184b3c37cbdfe9dfbfa104cb455e39
rahulRSoni <rahulsonisoni2015@gmail.com>
2023-12-05 08:54:09 +0530

    componant added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-
-
-
+import Header from "./component/Header/Header";
+import Footer from "./component/Footer/Footer";
+import { Outlet } from "react-router-dom";
 function App() {
-
-
   return (
     <>
-      
+      <Header />
+      <Outlet />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import Home from "./component/Home/Home.jsx";
+import About from "./component/About/About.jsx";
+import ContactUs from "./component/ContactUs/ContactUs.jsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "contactUs",
+        element: <ContactUs />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
